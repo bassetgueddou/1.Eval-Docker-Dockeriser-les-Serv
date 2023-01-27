a435959230f8ca3d371bad6b5e9bb7e30fbcbc8a
bassetgueddou <bassetgueddou@gmail.com>
2023-01-27 15:21:20 +0100

    workersspec

--- planner/main.js
+++ planner/main.js
@@ -12,9 +12,8 @@ const args = () => ({ a: randInt(0, 40), b: randInt(0, 40) })
 const generateTasks = (i) =>
   new Array(i).fill(1).map((_) => ({ type: taskType(), args: args() }))
 
-let workers = [
-  // { url: 'http://localhost:8080', id: '' }
-]
+  let workers = []
+
 
 const app = express()
 app.use(express.json())
@@ -41,42 +40,43 @@ let taskToDo = nbTasks
 const wait = (mili) =>
   new Promise((resolve, reject) => setTimeout(resolve, mili))
 
-const sendTask = async (worker, task) => {
-  console.log(`=> ${worker.url}/${task.type}`, task)
-  workers = workers.filter((w) => w.id !== worker.id)
-  tasks = tasks.filter((t) => t !== task)
-  const request = fetch(`${worker.url}/${task.type}`, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(task.args),
-  })
-    .then((res) => {
-      workers = [...workers, worker]
-      return res.json()
-    })
-    .then((res) => {
-      taskToDo -= 1
-      console.log('---')
-      console.log(nbTasks - taskToDo, '/', nbTasks, ':')
-      console.log(task, 'has res', res)
-      console.log('---')
-      return res
-    })
-    .catch((err) => {
-      console.error(task, ' failed', err.message)
-      tasks = [...tasks, task]
-    })
-}
+  const sendTask = async (task) => {
+    if (workers.length === 0 || tasks.length === 0) return
+    for (let i = 0; i < workers.length; i++) {
+      console.log(`=> ${workers[i]}/${task.type}`, task)
+      fetch(`${workers[i]}/${task.type}`, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(task.args),
+      })
+        .then((res) => {
+          taskToDo -= 1
+          console.log('---')
+          console.log(nbTasks - taskToDo, '/', nbTasks, ':')
+          console.log(task, 'has res', res)
+          console.log('---')
+          return res
+        })
+        .catch((err) => {
+          console.error(task, ' failed', err.message)
+          tasks = [...tasks, task]
+        })
+    }
+  }
+  
 
 const main = async () => {
   console.log(tasks)
   while (taskToDo > 0) {
     await wait(100)
-    if (workers.length === 0 || tasks.length === 0) continue
-    sendTask(workers[0], tasks[0])
+    if (workers.length === 0 || tasks.length === 0) continue;
+    for (let i = 0; i < workers.length; i++) {
+      sendTask(workers[i], tasks[i % tasks.length]);
+}
+
   }
   console.log('end of tasks')
   server.close()
